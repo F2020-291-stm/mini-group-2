17a0d00da0faddabe9f335543331ed8579e5bfce
Mackenzie Malainey <macmalainey@gmail.com>
2020-11-25 01:55:31 -0700

    Remove SimpleJson

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 from subprocess import check_call
 from sys import executable, argv
+from phase1 import build_database
 
 def install(package):
     check_call([executable, "-m", "pip", "install", package])
 
 if __name__ == "__main__":
     install("PyInquirer")
-    install("pymongo")
 
     # Handle execution of phase1
     if (len(argv) >= 2 and argv[1].isdigit()):
         print("A valid database port was given, running phase 1")
-
-        from phase1 import build_database
         build_database(int(argv[1]))
     else:
         print("Packages installed, please build database using phase1.py")
