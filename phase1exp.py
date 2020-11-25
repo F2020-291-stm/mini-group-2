91be66a67af3b5d36497164681b5692111cc34c6
Sandipan Nath <71223795+snath47@users.noreply.github.com>
2020-11-26 01:50:05 +0530

    timed

--- phase1exp.py
+++ phase1exp.py
@@ -1,9 +1,11 @@
 import build
 from sys import argv
+import time
 
 if __name__ == "__main__":
     if len(argv) < 2 or not argv[1].isdigit():
         print("No valid database port given, exiting...")
         exit()
-    
+    starttime = time.time()
     build.build_database(int(argv[1]))
+    print("------process takes " + time.time() - starttime)
